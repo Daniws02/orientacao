2152e0d3cd03b2965ef8f9eeafaf83b27072d1b4
Daniws02 <61484025+Daniws02@users.noreply.github.com>
2022-04-22 17:40:25 -0300

    Add files via upload

---
+++ orientacao/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace orientacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Pessoa P1 = new Pessoa();
+            Pessoa P2 = new Pessoa();
+            
+            P1.Nome = "Dan";
+            P2.Nome = "Den";
+
+            Console.WriteLine(P1.Nome);
+            Console.WriteLine(P2.Nome);
+
+        }
+    }
+    class Pessoa {
+
+        public string Nome;
+        public int Idade;
+        public int Altura;
+        public int Peso;
+
+    }
+}
