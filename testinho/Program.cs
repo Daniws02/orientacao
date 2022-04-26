5029eb12bc16997fc6a46538db7c6fdcbb1ab2b5
Daniws02 <santanadanilo576@gmail.com>
2022-04-26 17:10:03 -0300

    first commit

--- testinho/Program.cs
+++ testinho/Program.cs
@@ -6,7 +6,7 @@ namespace testinho
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Oie");
         }
     }
 }
